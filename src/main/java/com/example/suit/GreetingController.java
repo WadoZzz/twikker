1018659fbb21ddd90cde50653de533eaaee06682
Wadoz <vadikds@gmail.com>
2018-07-14 17:14:53 +0300

    add filter

--- src/main/java/com/example/suit/GreetingController.java
+++ src/main/java/com/example/suit/GreetingController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
 import java.util.Map;
 
 @Controller
@@ -35,6 +36,18 @@ public class GreetingController {
         messageRepo.save(message);
         Iterable<Message> all = messageRepo.findAll();
         model.put("message", all);
+        return "index";
+    }
+
+    @PostMapping("filter")
+    public String filter(@RequestParam String tag, Map<String, Object> model) {
+        Iterable<Message> byTag;
+        if (tag != null && !tag.isEmpty()) {
+            byTag = messageRepo.findByTag(tag);
+        } else {
+            byTag = messageRepo.findAll();
+        }
+        model.put("message", byTag);
         return "index";
     }
 
